7ac05fb4d23c65f956fd9bf272779daa92b44832
Camila <costamilla17@gmail.com>
2019-05-29 21:30:58 -0300

    ryrry

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
+    path('meu_blog', views.meu_blog, name='meu_blog'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
+
 
 ]
