4704bcf02c7e034155f2cb8b6f09d5c8eb380223
Camila <costamilla17@gmail.com>
2019-06-03 21:29:27 -0300

    alteração dia 03/06

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('meu_blog', views.meu_blog, name='meu_blog'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new', views.post_new, name='post_new'),
 
 
 ]
